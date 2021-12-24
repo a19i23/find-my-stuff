a29b01d102c3c7c7156ba72d8ed28f96c2ba5928
Alan Villa <a19i23@hotmail.com>
2021-12-24 13:09:59 -0800

    adding eslint config

--- pages/items.js
+++ pages/items.js
@@ -1,27 +1,25 @@
-import { connectToDatabase } from "../util/mongodb";
+import { connectToDatabase } from '../util/mongodb';
 import moment from 'moment';
 
 export default function Items({ items }) {
-  
   return (
     <div>
       <h1>My Items</h1>
-     
+
       <ul>
         {items.map((item) => {
-          const dbDate = item.lastupdated
-          const formattedDate = moment(dbDate).format('MM-DD-YYYY')
+          const dbDate = item.lastupdated;
+          const formattedDate = moment(dbDate).format('MM-DD-YYYY');
 
           return (
-            <li>
+            <li key={item._id}>
               <h2>Name: {item.name}</h2>
               <h3>Location: {item.location}</h3>
               <p>Box number: {item.boxNumber}</p>
               <p>Date updated: {formattedDate}</p>
             </li>
-          )
-          }
-        )}
+          );
+        })}
       </ul>
     </div>
   );
@@ -30,10 +28,7 @@ export default function Items({ items }) {
 export async function getServerSideProps() {
   const { db } = await connectToDatabase();
 
-  const items = await db
-    .collection("items")
-    .find({})
-    .toArray();
+  const items = await db.collection('items').find({}).toArray();
 
   return {
     props: {
