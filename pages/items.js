8038bec24eda398846f1aa7b4600fd3235837e30
Alan Villa <a19i23@hotmail.com>
2021-12-25 19:12:05 -0800

    adding tailwind css

--- pages/items.js
+++ pages/items.js
@@ -3,25 +3,87 @@ import moment from 'moment';
 
 export default function Items({ items }) {
   return (
-    <div>
-      <h1>My Items</h1>
+    <>
+      <div className="font-bold text-xl mb-2 ml-20 mt-10">My Items</div>
 
-      <ul>
-        {items.map((item) => {
-          const dbDate = item.lastupdated;
-          const formattedDate = moment(dbDate).format('MM-DD-YYYY');
+      <div className="flex justify-center items-center">
+        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Name
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Location
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Box Number
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Last Updated
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200">
+                  {items.map((item) => {
+                    const dbDate = item.lastUpdated;
+                    const formattedDate = moment(dbDate).format('MMMM Do YYYY');
+                    const formattedTime = moment(dbDate).format('h:mm:ss a');
 
-          return (
-            <li key={item._id}>
-              <h2>Name: {item.name}</h2>
-              <h3>Location: {item.location}</h3>
-              <p>Box number: {item.boxNumber}</p>
-              <p>Date updated: {formattedDate}</p>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+                    return (
+                      <tr key={item._id}>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="flex items-center">
+                            <div className="ml-4">
+                              <div className="text-sm font-medium text-gray-900">
+                                {item.name}
+                              </div>
+                            </div>
+                          </div>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="text-sm text-gray-900">
+                            {item.location}
+                          </div>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="text-sm text-gray-900 text-center">
+                            {item.boxNumber}
+                          </div>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                          <div className="text-sm text-gray-500">
+                            {formattedDate}
+                          </div>
+                          <div className="text-sm text-gray-500">
+                            {formattedTime}
+                          </div>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
