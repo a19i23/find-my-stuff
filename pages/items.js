fb971748a93d94455bb80abb678a1c3b4f36a4ae
Alan Villa <a19i23@hotmail.com>
2021-12-26 21:03:39 -0800

    adding dark option

--- pages/items.js
+++ pages/items.js
@@ -3,43 +3,45 @@ import moment from 'moment';
 
 export default function Items({ items }) {
   return (
-    <>
-      <div className="font-bold text-xl mb-2 ml-20 mt-10">My Items</div>
+    <div className="bg-white dark:bg-gray-900" style={{ height: '100vh' }}>
+      <div className="text-gray-900 dark:text-white font-bold text-xl px-8 pt-8 pb-2">
+        My Items
+      </div>
 
       <div className="flex justify-center items-center">
         <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-              <table className="min-w-full divide-y divide-gray-200">
-                <thead className="bg-gray-50">
+            <div className="shadow-md overflow-hidden border-b border-gray-200 dark:border-gray-800 sm:rounded-lg">
+              <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-600">
+                <thead className="bg-gray-50 dark:bg-gray-500">
                   <tr>
                     <th
                       scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-white uppercase tracking-wider"
                     >
                       Name
                     </th>
                     <th
                       scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-white uppercase tracking-wider"
                     >
                       Location
                     </th>
                     <th
                       scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-white uppercase tracking-wider"
                     >
                       Box Number
                     </th>
                     <th
                       scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-white uppercase tracking-wider"
                     >
                       Last Updated
                     </th>
                   </tr>
                 </thead>
-                <tbody className="bg-white divide-y divide-gray-200">
+                <tbody className="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-600">
                   {items.map((item) => {
                     const dbDate = item.lastUpdated;
                     const formattedDate = moment(dbDate).format('MMMM Do YYYY');
@@ -50,27 +52,27 @@ export default function Items({ items }) {
                         <td className="px-6 py-4 whitespace-nowrap">
                           <div className="flex items-center">
                             <div className="ml-4">
-                              <div className="text-sm font-medium text-gray-900">
+                              <div className="text-sm font-medium text-gray-900 dark:text-gray-200">
                                 {item.name}
                               </div>
                             </div>
                           </div>
                         </td>
                         <td className="px-6 py-4 whitespace-nowrap">
-                          <div className="text-sm text-gray-900">
+                          <div className="text-sm text-gray-900 dark:text-gray-400">
                             {item.location}
                           </div>
                         </td>
                         <td className="px-6 py-4 whitespace-nowrap">
-                          <div className="text-sm text-gray-900 text-center">
+                          <div className="text-sm text-gray-900 text-center dark:text-gray-400">
                             {item.boxNumber}
                           </div>
                         </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                          <div className="text-sm text-gray-500">
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-white">
+                          <div className="text-sm text-gray dark:text-gray-400">
                             {formattedDate}
                           </div>
-                          <div className="text-sm text-gray-500">
+                          <div className="text-sm text-gray-500 dark:text-gray-400">
                             {formattedTime}
                           </div>
                         </td>
@@ -83,7 +85,7 @@ export default function Items({ items }) {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
