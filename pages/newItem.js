d90305f844606def704fc414c1059ad7cf8e3aaa
Alan Villa <a19i23@hotmail.com>
2021-12-28 14:50:27 -0800

    using tailwind for new item input

--- pages/newItem.js
+++ pages/newItem.js
@@ -9,7 +9,6 @@ export default function Movies() {
       boxNumber: e.target.boxNum.value,
       lastUpdated: date,
     };
-    console.log(item);
 
     const response = await fetch('/api/newItem', {
       method: 'POST',
@@ -19,27 +18,97 @@ export default function Movies() {
       },
     });
     const data = await response.json();
-    console.log(data);
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="itemName">Item name</label>
-        <br />
-        <input type="text" id="itemName" name="itemName" />
-        <br />
-        <label htmlFor="itemLocation">Item location</label>
-        <br />
-        <input type="text" id="itemLocation" name="itemLocation" />
-        <br />
-        <label htmlFor="boxNum">Box number</label>
-        <br />
-        <input type="text" id="boxNum" name="boxNum" />
-        <br />
-        <br />
-        <button type="submit">Submit</button>
-      </form>
+    <div
+      className="flex justify-center items-center bg-white dark:bg-gray-900"
+      style={{ height: '100vh' }}
+    >
+      <div className="mt-10 mx-4 sm:mt-4 sm:mx-10">
+        <div className="md:grid md:grid-cols-3 md:gap-6">
+          <div className="md:col-span-1">
+            <div className="px-4 sm:px-0">
+              <h3 className="text-lg font-medium leading-6 text-gray-900 dark:text-white">
+                New Item
+              </h3>
+              <p className="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                Add an item that will be tracked in the inventory.
+              </p>
+            </div>
+          </div>
+          <div className="mt-5 md:mt-0 md:col-span-2">
+            <form onSubmit={handleSubmit}>
+              <div className="shadow overflow-hidden rounded-md">
+                <div className="px-4 py-5 bg-white dark:bg-gray-700 sm:p-6">
+                  <div className="grid grid-cols-6 gap-6">
+                    <div className="col-span-6 sm:col-span-3">
+                      <label
+                        htmlFor="itemName"
+                        className="block text-sm font-medium text-gray-700 dark:text-gray-200"
+                      >
+                        Item name
+                      </label>
+                      <input
+                        type="text"
+                        name="itemName"
+                        id="itemName"
+                        className="mt-1 py-2 px-3 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      />
+                    </div>
+
+                    <div className="col-span-6 sm:col-span-4">
+                      <label
+                        htmlFor="itemLocation"
+                        className="block text-sm font-medium text-gray-700 dark:text-gray-200"
+                      >
+                        Item location
+                      </label>
+                      <select
+                        type="text"
+                        name="itemLocation"
+                        id="itemLocation"
+                        className="mt-1 py-2 px-3 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      >
+                        <option>Wall (Back)</option>
+                        <option>Wall (Middle)</option>
+                        <option>Wall (Front)</option>
+                        <option>TV Stand (Bottom)</option>
+                        <option>TV Stand (Top)</option>
+                        <option>Bookcase</option>
+                        <option>Center (Front)</option>
+                      </select>
+                    </div>
+
+                    <div className="col-span-6 sm:col-span-2">
+                      <label
+                        htmlFor="boxNum"
+                        className="block text-sm font-medium text-gray-700 dark:text-gray-200"
+                      >
+                        Box number
+                      </label>
+                      <input
+                        type="text"
+                        name="boxNum"
+                        id="boxNum"
+                        className="mt-1 py-2 px-3 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="px-4 py-3 bg-gray-50 dark:bg-gray-800 text-right sm:px-6">
+                  <button
+                    type="submit"
+                    className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    Save
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
