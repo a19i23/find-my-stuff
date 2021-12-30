ad8de999e0a25f461d4b6c26df894aa8324717ee
Alan Villa <a19i23@hotmail.com>
2021-12-29 16:43:20 -0800

    adding search to table and styling

--- pages/items.js
+++ pages/items.js
@@ -1,14 +1,42 @@
+import React, { useState, useEffect } from 'react';
 import { connectToDatabase } from '../util/mongodb';
 import moment from 'moment';
 
 export default function Items({ items }) {
+  const [search, setSearch] = useState('');
+  const [tableItems, setTableItems] = useState();
+
+  useEffect(() => {
+    if (search) {
+      const filteredItems = items?.filter(
+        (item) =>
+          item.name.toLowerCase().includes(search.toLowerCase()) ||
+          item.location.toLowerCase().includes(search.toLowerCase()),
+      );
+      setTableItems(filteredItems);
+    } else {
+      setTableItems(items);
+    }
+  }, [items, search]);
+
   return (
     <div className="bg-white dark:bg-gray-900" style={{ height: '100vh' }}>
       <div className="flex justify-center items-center">
         <div className="overflow-x-auto mt-10 mx-4 sm:mx-10">
           <div className="align-middle inline-block min-w-full ">
-            <div className="text-gray-900 dark:text-white font-bold text-xl px-8 pt-8 pb-2">
-              My Items
+            <div className="flex justify-between items-baseline">
+              <div className="text-gray-900 dark:text-white font-bold text-xl px-8 pt-8 pb-2">
+                My Items
+              </div>
+              <input
+                type="text"
+                name="search"
+                id="search"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                className="h-6 px-1 mr-1 bg-white dark:bg-gray-500 text-gray-900 dark:text-gray-300 outline-none rounded-md"
+                placeholder="Search..."
+              />
             </div>
             <div className="shadow-md h-96 overflow-y-scroll overflow-hidden border-b border-gray-200 dark:border-gray-800 sm:rounded-lg">
               <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-600">
@@ -41,7 +69,7 @@ export default function Items({ items }) {
                   </tr>
                 </thead>
                 <tbody className="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-600">
-                  {items.map((item) => {
+                  {tableItems?.map((item) => {
                     const dbDate = item.lastUpdated;
                     const formattedDate = moment(dbDate).format('MMMM Do YYYY');
                     const formattedTime = moment(dbDate).format('h:mm:ss a');
@@ -51,14 +79,14 @@ export default function Items({ items }) {
                         <td className="px-6 py-4 whitespace-nowrap">
                           <div className="flex items-center">
                             <div className="ml-4">
-                              <div className="text-sm font-medium text-gray-900 dark:text-gray-200">
+                              <div className="w-52 whitespace-pre-wrap text-sm font-medium text-gray-900 dark:text-gray-200 ">
                                 {item.name}
                               </div>
                             </div>
                           </div>
                         </td>
                         <td className="px-6 py-4 whitespace-nowrap">
-                          <div className="text-sm text-gray-900 dark:text-gray-400">
+                          <div className="w-24 text-sm text-gray-900 dark:text-gray-400">
                             {item.location}
                           </div>
                         </td>
