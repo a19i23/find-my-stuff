1d4ae3028b419e182c33e70b10a9452df3ec1521
Alan Villa <a19i23@hotmail.com>
2022-02-11 20:22:19 -0800

    adding login to index

---
+++ components/LoginBar.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function LoginBar() {
+  return (
+    <>
+      <AppBar className="bg-white dark:bg-gray-900">
+        <Toolbar>
+          <Typography
+            className="font-light"
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1 }}
+          >
+            findMyStuff
+          </Typography>
+          <Button className="font-light" href="/api/login" color="inherit">
+            Login
+          </Button>
+          <Button className="font-light" href="/api/logout" color="inherit">
+            Logout
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+}
