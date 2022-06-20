021573a291a7523c678f8b3c8c013f5c625af2d5
Alan Villa <a19i23@hotmail.com>
2022-06-19 20:49:05 -0700

    defining locations in other file

---
+++ pages/locations.js
@@ -0,0 +1,14 @@
+export const cudahyLocations = [
+  'Wall (Back)',
+  'Wall (Middle)',
+  'Wall (Front)',
+  'TV Stand (Bottom)',
+  'TV Stand (Top)',
+  'Bookcase',
+  'Center (Front)',
+];
+
+export const downeyGarage = {
+  areas: ['Left (East)', 'Back (South)', 'Right (West)'],
+  levels: ['Top', 'Middle', 'Bottom', 'Floor'],
+};
