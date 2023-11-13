c81a4398654b1c6f5d01c23b6c0108b6dc5fcd21
Alan Villa <a19i23@hotmail.com>
2023-11-12 22:43:54 -0800

    adding redirect in callback.js

--- pages/api/callback.js
+++ pages/api/callback.js
@@ -1,10 +1,13 @@
-import auth0 from '../../lib/auth0';
+import auth0 from '@auth0/nextjs-auth0';
 
 export default async function callback(req, res) {
   try {
     await auth0.handleCallback(req, res);
   } catch (error) {
     console.error(error);
-    res.status(error.status || 500).end(error.message);
+    res
+      .status(error.status || 500)
+      .end(error.message)
+      .redirect('/');
   }
 }
